e83b2f33299ba5eb31f699b57fea3c91fb614621
IkkiArklabs <ikki@arklabs.tech>
2025-04-03 01:12:24 +0800

    modify css2

--- pages/components/Container.js
+++ pages/components/Container.js
@@ -2,7 +2,7 @@ const Container = ({ children, marginTop = "4rem", marginBottom = "0rem" }) => {
   return (
     <div
       style={{ marginTop, marginBottom }}
-      className="relative flex sm:flex-col sm:gap-y-6 lg:flex-row lg:gap-x-6  w-[65vw] mr-auto ml-auto max-w-[1300px] sm:w-[100vw] sm:max-w-[100%]"
+      className="relative flex sm:flex-col sm:gap-y-6 lg:flex-row lg:gap-x-6  w-[65vw] mr-auto ml-auto max-w-[1300px] sm:w-[80vw]"
     >
       {children}
     </div>
