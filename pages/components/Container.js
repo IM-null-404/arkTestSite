4a86ca50763aa111bd374132e56a5ccd56ad9c51
IkkiArklabs <ikki@arklabs.tech>
2025-04-03 03:02:20 +0800

    mCss5

--- pages/components/Container.js
+++ pages/components/Container.js
@@ -2,7 +2,7 @@ const Container = ({ children, marginTop = "4rem", marginBottom = "0rem" }) => {
   return (
     <div
       style={{ marginTop, marginBottom }}
-      className="relative flex sm:flex-col sm:gap-y-6 lg:flex-row lg:gap-x-6  w-[65vw] mr-auto ml-auto max-w-[1300px] sm:w-[80vw]"
+      className="relative w-[65vw] mr-auto ml-auto max-w-[1300px] flex sm:flex-col sm:gap-y-6 xs:flex-col xs:gap-y-6 xs:w-[85vw] lg:flex-row lg:gap-x-6  "
     >
       {children}
     </div>
